0237a7fcdecd628c2a48090c3d05b013adb64531
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-14 17:11:44 +0100

    commit

--- ejercicio1/archivo.php
+++ ejercicio1/archivo.php
@@ -1,37 +1,10 @@
 <?php
-
-// Data Base Connect
-$servername = "localhost";
-$username = "root";
-$password = "symfony";
-$dbname = "clase";
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $nombre = $_POST['nombre'];
-    $apellido = $_POST['apellido'];
-
-    $sql = "INSERT INTO personas (nombre, apellido) VALUES ('$nombre', '$apellido')";
-
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(["mensaje" => "Usuario añadido correctamente"]);
+    if (isset($_GET['name']) && is_string($_GET['name']) && isset($_GET['lastName']) && is_string($_GET['lastName'])) {
+        $name = $_GET['name'];
+        $lastName = $_GET['lastName'];
+        $fullName = $name . ' - ' . $lastName;
+        echo $fullName;
     }else{
-        echo json_encode(["mensaje" => "Error: ".$sql . "<br>" . $conn->error]);
+        echo "Introduce nombre y apellido";
     }
-}else if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-    $sql = "SELECT * FROM personas";
-    $result = $conn->query($sql);
-    $personas = [];
-
-    while ($row = $result->fetch_assoc()) {
-        $personas[] = $row;
-    }
-    echo json_encode($personas);
-}
-$conn->close();
 ?>
--- ejercicio1/js/script.js
+++ ejercicio1/js/script.js
@@ -1,19 +1,22 @@
 document.addEventListener("DOMContentLoaded",function () {
     const xhr = new XMLHttpRequest();
-    const form = document.querySelector("form");
-    form.addEventListener()
+    const fullName = document.getElementById("fullName");
+    const getForm = document.getElementById("getForm");
 
-    const obtenerUsuarios = document.getElementById("obtenUsuarios");
-    const filePhp = 'archivo.php';
-    obtenerUsuarios.addEventListener("click",function (event) {
-       event.preventDefault();
 
-       xhr.open("GET",filePhp,true);
-       xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-
+    getForm.addEventListener("submit",function () {
+        var name = document.getElementById("name").value;
+        var lastName = document.getElementById("lastName").value;
+        
+        const file = 'archivo.php';
+        const parametros = `?name=${encodeURIComponent(name)}&lastName=${encodeURIComponent(lastName)}`; 
+       
+        xhr.open("GET",file + parametros,true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                fullName.value = xhr.responseText;
             }
-       };
-       xhr.send();
+        }
+        xhr.send();
     });
 });
